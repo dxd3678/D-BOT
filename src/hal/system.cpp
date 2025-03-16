262f9ae8940d577bad74835feed4424748bd7c7f
dingmos <db_smpond@163.com>
2025-03-16 20:48:33 +0800

    [hal] update system ui display, but it still has some issues.

--- src/hal/system.cpp
+++ src/hal/system.cpp
@@ -48,34 +48,37 @@ static const lv_color_t log_colors[] = {
     LV_COLOR_MAKE(0, 180, 255)  // DEBUG - 青色
 };
 
-static lv_obj_t *log_area;
+static lv_obj_t *g_log_area;
 
 void HAL::log_system(int level, const char *fmt, ...)
 {
     char buffer[256];
     va_list args;
+
+    va_start(args, fmt);
+    lv_vsnprintf(buffer, sizeof(buffer), fmt, args);
+    va_end(args);
+    /** log to serial */
+    log_i("%s", buffer);
+
+    if (!g_log_area) {
+        return;
+    }
     
     if(level < 0 || level >= sizeof(log_colors)/sizeof(log_colors[0])) {
         level = SYSTEM_ERR;
     }
 
-    va_start(args, fmt);
-    lv_vsnprintf(buffer, sizeof(buffer), fmt, args);
-    va_end(args);
-
-    lv_textarea_add_text(log_area, buffer);
-    lv_textarea_add_char(log_area, '\n');
+    lv_textarea_add_text(g_log_area, buffer);
+    lv_textarea_add_char(g_log_area, '\n');
     
-    // lv_obj_set_style_text_color(lv_textarea_get_label(log_area), 
+    // lv_obj_set_style_text_color(lv_textarea_get_label(g_log_area), 
                             //   log_colors[level], 
                             //   LV_PART_SELECTED);
     
-    // lv_obj_scroll_to_view_recursive(lv_textarea_get_label(log_area), LV_ANIM_OFF);
+    // lv_obj_scroll_to_view_recursive(lv_textarea_get_label(g_log_area), LV_ANIM_OFF);
 
-    /** log to serial */
-    log_i("%s", buffer);
-
-    lv_refr_now(NULL);
+    // lv_refr_now(NULL);
 }
 
 int HAL::system_ui_init(void) 
@@ -84,23 +87,22 @@ int HAL::system_ui_init(void)
 
     lv_obj_set_style_bg_color(screen, lv_color_black(), LV_PART_MAIN);
 
-    log_area = lv_textarea_create(screen);
-    lv_obj_set_size(log_area, CONFIG_SCREEN_HOR_RES - 40 , CONFIG_SCREEN_VER_RES - 90);
-    lv_obj_align(log_area, LV_ALIGN_CENTER, 0, 0);
+    g_log_area = lv_textarea_create(screen);
+    lv_obj_set_size(g_log_area, CONFIG_SCREEN_HOR_RES - 40 , CONFIG_SCREEN_VER_RES - 90);
+    lv_obj_align(g_log_area, LV_ALIGN_CENTER, 0, 0);
 
-    lv_obj_set_style_bg_opa(log_area, LV_OPA_0, LV_PART_MAIN);
-    lv_obj_set_style_text_color(log_area, lv_color_white(), LV_PART_MAIN);
-    lv_obj_set_style_border_width(log_area, 0, LV_PART_MAIN);
+    lv_obj_set_style_bg_opa(g_log_area, LV_OPA_0, LV_PART_MAIN);
+    lv_obj_set_style_text_color(g_log_area, lv_color_white(), LV_PART_MAIN);
+    lv_obj_set_style_border_width(g_log_area, 0, LV_PART_MAIN);
 
-    lv_textarea_set_text(log_area, "booting system...\n");
+    lv_textarea_set_text(g_log_area, "booting system...\n");
     return 0;
 }
 
 int HAL::system_ui_uninit()
 {
-    // if (g_log_buffer) {
-    //     free(g_log_buffer);
-    // }
-    lv_obj_clean(lv_scr_act());
+    // lv_obj_clean(lv_scr_act());
+    // lv_textarea_set_text(g_log_area, "");
+    g_log_area = NULL;
     return 0;
 }
